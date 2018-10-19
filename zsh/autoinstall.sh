c7d83493544b4f6cb917fa40322f500b83463388
Wattanit Hotrakool <wattanit@eatlab.space>
2018-10-19 11:53:08 +0700

    Install z.sh

--- zsh/autoinstall.sh
+++ zsh/autoinstall.sh
@@ -5,5 +5,9 @@ echo "Installing ZSH and Wattanit's ZSH configurations"
 # Install Oh-my-zsh https://ohmyz.sh/
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# Install z.sh
+wget https://raw.githubusercontent.com/rupa/z/master/z.sh
+mv z.sh $HOME/.oh-my-zsh/z.sh
+
 # Install .zshrc configuration
 cp .zshrc $HOME/.zshrc 
